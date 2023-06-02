141253044446c687a46ddc6b9da09aaa35f668f4
ibrahim Girişken <ibrahimgirisken@hotmail.com>
2023-06-02 14:00:46 +0300

    02-06-2023

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,7 +35,7 @@ export function HttpLoaderFactory(httpClient: HttpClient) {
         deps: [HttpClient]
       },
       defaultLanguage: "tr"
-    })
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
