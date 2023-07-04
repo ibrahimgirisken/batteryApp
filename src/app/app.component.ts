53a6a9acb004b96e3259974d176b96934fe8cd63
ibrahim Girişken <ibrahimgirisken@hotmail.com>
2023-07-04 08:45:50 +0300

    04-07-2023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, OnInit } from '@angular/core';
 
-declare var $: any
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
+export class AppComponent{
 
 }
